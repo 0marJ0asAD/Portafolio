b88cfab9062983c5faa05e24cc40866085aa189b
Omar Aguilar Dávila <113153306+0marJ0asAD@users.noreply.github.com>
2022-09-26 03:07:20 -0500

    Add files via upload

---
+++ Portafolio/Presentación/JavaScript/GSAP.js
@@ -0,0 +1,296 @@
+/* Ánimaciones de GSAP */
+
+gsap.to('.Logotipo-Desktop',{
+    duration: 2,
+    rotate: 360,
+    x: 630,
+    ease: 'bounce',
+});
+
+gsap.to('.NombreCompleto-Desktop',{
+    duration: 3,
+    delay: 2,
+    x: 510,
+    ease: 'bounce',
+});
+
+gsap.to('.LoQueCreoQueSoyYLoQueMeFalta-PC',{
+    duration: 4,
+    delay: 5,
+    x: 560,
+    ease: 'bounce',
+});
+
+gsap.to('.GitHub-Desktop',{
+    duration: 5,
+    delay: 9,
+    x: 615,
+    ease: 'bounce',
+}); '+=2'
+
+gsap.to('.TítuloDeLoQueSé-Desktop',{
+    duration: 2,
+    rotate: 360,
+    x: 1350,
+    y: -425,
+    ease: 'bounce',
+});
+
+gsap.to('.HTML5-Desktop',{
+    duration: 3,
+    delay: 2,
+    rotate: 360,
+    x: 1350,
+    y: -375,
+    ease: 'bounce',
+});
+
+gsap.to('.CSS3-Desktop',{
+    duration: 4,
+    delay: 3,
+    rotate: 360,
+    x: 1350,
+    y: -375,
+    ease: 'bounce',
+});
+
+gsap.to('.JavaScript-Desktop',{
+    duration: 5,
+    delay: 4,
+    rotate: 360,
+    x: 1350,
+    y: -375,
+    ease: 'bounce',
+});
+
+gsap.to('.Python-Desktop',{
+    duration: 6,
+    delay: 5,
+    rotate: 360,
+    x: 1350,
+    y: -375,
+    ease: 'bounce',
+});
+
+gsap.to('.Developer-Desktop',{
+    duration: 7,
+    delay: 6,
+    rotate: 360,
+    x: -800,
+    y: -820,
+    ease: 'bounce',
+});
+
+gsap.to('.Inglés-Desktop',{
+    duration: 8,
+    delay: 7,
+    rotate: 360,
+    x: -1260,
+    y: -520,
+    ease: 'bounce',
+});
+
+gsap.to('.FrameWorks-Desktop',{
+    duration: 9,
+    delay: 8,
+    x:1000,
+    ease: 'bounce',
+});
+
+gsap.timeline('.FrameWorks-Desktop',{
+    rotate: 360,
+    x: -1000,
+    ease: 'bounce',
+});
+
+gsap.to('.Logotipo-Tablet',{
+    duration: 2,
+    rotate: 360,
+    x: 630,
+    ease: 'bounce',
+});
+
+gsap.to('.NombreCompleto-Tablet',{
+    duration: 3,
+    delay: 2,
+    x: 500,
+    ease: 'bounce',
+});
+
+gsap.to('.LoQueCreoQueSoyYLoQueMeFalta-Tab',{
+    duration: 4,
+    delay: 5,
+    x: 560,
+    ease: 'bounce',
+});
+
+gsap.to('.GitHub-Tablet',{
+    duration: 5,
+    delay: 9,
+    x: 610,
+    ease: 'bounce',
+}); '+=2'
+
+gsap.to('.TítuloDeLoQueSé-Tablet',{
+    duration: 2,
+    rotate: 360,
+    x: 1100,
+    y: -425,
+});
+
+gsap.to('.HTML5-Tablet',{
+    duration: 3,
+    delay: 2,
+    rotate: 360,
+    x: 1100,
+    y: -375,
+});
+
+gsap.to('.CSS3-Tablet',{
+    duration: 4,
+    delay: 3,
+    rotate: 360,
+    x: 1100,
+    y: -375,
+});
+
+gsap.to('.JavaScript-Tablet',{
+    duration: 5,
+    delay: 4,
+    rotate: 360,
+    x: 1100,
+    y: -375,
+});
+
+gsap.to('.Python-Tablet',{
+    duration: 6,
+    delay: 5,
+    rotate: 360,
+    x: 1100,
+    y: -375,
+});
+
+gsap.to('.Developer-Tablet',{
+    duration: 7,
+    delay: 6,
+    rotate: 360,
+    x: -800,
+    y: -820,
+});
+
+gsap.to('#TextoEnglish',{
+    duration: 8,
+    delay: 7,
+    rotate: 360,
+    x: -1200,
+    y: -520,
+});
+
+gsap.to('#ImagenEnglish',{
+    duration: 8,
+    delay: 7,
+    rotate: 360,
+    x: -1140,
+    y: -513,
+});
+
+gsap.to('.FrameWorks-Tablet',{
+    duration: 9,
+    delay: 8,
+    x:1000,
+});
+
+gsap.to('.Logotipo-Móvil',{
+    duration: 2,
+    rotate: 360,
+    x: 140,
+});
+
+gsap.to('.NombreCompleto-Móvil',{
+    duration: 3,
+    delay: 2,
+    x: 107,
+    ease: 'bounce',
+});
+
+gsap.to('.LoQueCreoQueSoyYLoQueMeFalta-Mov',{
+    duration: 4,
+    delay: 5,
+    x: 120,
+    ease: 'bounce',
+});
+
+gsap.to('.GitHub-Móvil',{
+    duration: 5,
+    delay: 10,
+    x: 133,
+    ease: 'bounce',
+});
+
+gsap.to('.TítuloDeLoQueSé-Móvil',{
+    duration: 2,
+    rotate: 360,
+    x: 183,
+    y: -150,
+});
+
+gsap.to('.HTML-Móvil',{
+    duration: 3,
+    delay: 2,
+    rotate: 360,
+    x: 233,
+    y: -150,
+});
+
+gsap.to('.CSS-Móvil',{
+    duration: 4,
+    delay: 5,
+    rotate: 360,
+    x: 233,
+    y: -150,
+});
+
+gsap.to('.JavaScript-Móvil',{
+    duration: 5,
+    delay: 10,
+    rotate: 360,
+    x: 233,
+    y: -150,
+});
+
+gsap.to('.Python-Móvil',{
+    duration: 6,
+    delay: 15,
+    rotate: 360,
+    x: 233,
+    y: -150,
+});
+
+gsap.to('.GSAP-Móvil',{
+    duration: 4,
+    delay: 5,
+    x: -225,
+    ease: 'bounce',
+});
+
+gsap.to('.AngularJS-Móvil',{
+    duration: 5,
+    delay: 10,
+    x: -225,
+    ease: 'bounce',
+});
+
+gsap.to('.SASS-Móvil',{
+    duration: 6,
+    delay: 15,
+    x: -225,
+    ease: 'bounce',
+});
+
+gsap.to('.Developer-Móvil',{
+    duration: 4,
+    delay: 5,
+    rotate: 360,
+    x: -130,
+    y: -143,
+});
